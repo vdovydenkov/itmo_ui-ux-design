edd54c8132f20f8f807e7181853afef658dc9cf3
VDovydenkov <dov@mlbox.ru>
2025-04-21 15:26:31 +0300

    Добавлен маршрут регистрации и общая переменная для заголовков.

--- date_nexus/app.py
+++ date_nexus/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+# Заголовок для всех страниц
+app_title = 'DATE NEXUS'
 
 app = Flask(__name__)
 
+# Индексная страница
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', app_title=app_title)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Прислали логин и пароль
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    # Посмотрим
+    return f"Email: {email}, Пароль: {password}"
+
+# Страница регистрации
+@app.route('/register')
+def register():
+    return render_template('register.html', app_title=app_title)
+
+# Запускаем
+app.run(debug=True)
