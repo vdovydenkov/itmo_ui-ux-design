1f7dc86c314e7d54783810378dd857011998e712
VDovydenkov <dov@mlbox.ru>
2025-05-11 17:13:57 +0300

    Реализована "настоящая" сессия в замен псевдо-сессии.

--- date_nexus/__main__.py
+++ date_nexus/__main__.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from date_nexus.services.databaser import get_user_by_email, get_calendars_by_user_id, get_events_by_calendar
 
 # Заголовок для всех страниц
 app_title = 'DATE NEXUS'
-# Псевдосессия
-pseudo_session = None
 
 app = Flask(__name__)
+app.secret_key = '1,2,3,4,5,5.4.3.2.1' # Понятно, что так нельзя
 
 # Индексная страница
 @app.route('/')
@@ -16,8 +15,6 @@ def home():
 # Прислали логин и пароль
 @app.route('/login', methods=['POST'])
 def login():
-    global pseudo_session 
-
     user_email = request.form['email']
     user_password = request.form['password']
     # Проверяем пользователя
@@ -36,13 +33,11 @@ def login():
     # Грузим все события календарей
     for calendar in calendars:
         events.extend(get_events_by_calendar(calendar.get('id')))
-    if not pseudo_session:
-        pseudo_session = {}
     # Грузим данные в сессию
-    pseudo_session['user'] = user
-    pseudo_session['calendars'] = calendars
-    pseudo_session['events'] = events
-    print(f'Загрузили в сессию: pseudo_session={pseudo_session}')
+    session['user'] = user
+    session['calendars'] = calendars
+    session['events'] = events
+    print('Загрузили в сессию')
     # Переходим к основному окну событий и календарей
     return redirect(url_for('events'))
 
@@ -81,19 +76,19 @@ def confirm_email():
 # Переключение флажка календаря
 @app.route('/toggle_calendar', methods=['POST'])
 def toggle_calendar():
-    global pseudo_session
-
     # Читаем из формы
     calendar_id = request.form.get('calendar_id')
     # Если checkbox отмечен, в форме приходит selected=='1', иначе параметр отсутствует
     selected = 'selected' in request.form
 
-    # Обновляем статус в pseudo_session
-    if pseudo_session and 'calendars' in pseudo_session:
-        for calendar in pseudo_session['calendars']:
+    # Обновляем статус в session
+    if 'calendars' in session:
+        calendars = session['calendars']
+        for calendar in calendars:
             if str(calendar.get('id')) == calendar_id:
                 calendar['selected'] = selected
                 break
+        session['calendars'] = calendars  # Обновляем в сессии
 
     # После изменения перенаправляем обратно на страницу events
     return redirect(url_for('events'))
@@ -101,14 +96,14 @@ def toggle_calendar():
 # Основное окно событий и календарей
 @app.route('/events')
 def events():
-    if not pseudo_session:
+    if 'user' not in session or 'calendars' not in session:
         print('events: нет сессии.')
         # Отправляемся на страницу входа
         return render_template('index.html', app_title=app_title, app_message='Пользователь не авторизирован.')
     # Берём пользователя, календари и события из сессии
-    user = pseudo_session['user']
-    calendars = pseudo_session['calendars']
-    events = pseudo_session['events']
+    user = session.get('user')
+    calendars = session.get('calendars', [])
+    events = session.get('events', [])
     if not user:
         print('Events: Пользователь не залогинен.')
         # Отправляемся на страницу входа
@@ -132,16 +127,16 @@ def events():
     # Передача данных в шаблон
     return render_template('events.html', app_title=app_title, events=selected_events, calendars=calendars, user_name=user_name)
 
+# Добавление события
 @app.route('/add_event', methods=['GET', 'POST'])
 def add_event():
-    global pseudo_session
-    if not pseudo_session or 'user' not in pseudo_session:
+    if 'user' not in session:
         flash('Пользователь не авторизирован.')
         return redirect(url_for('home'))
 
-    user = pseudo_session['user']
-    calendars = pseudo_session.get('calendars', [])
-    events = pseudo_session.get('events', [])
+    user = session.get('user')
+    calendars = session.get('calendars', [])
+    # events = session.get('events', [])
 
     if request.method == 'POST':
         # Считываем данные из формы
@@ -173,8 +168,8 @@ def add_event():
             'calendar_id': int(calendar_id),
         }
 
-        # Сохраняем в events
-        events.append(event_data)
+        # Сохраняем в сессию
+        session['events'].append(event_data)
 
         flash('Событие успешно добавлено!')
         return redirect(url_for('events'))
